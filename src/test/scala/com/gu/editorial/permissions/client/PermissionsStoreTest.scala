e9176f42f21bb0de345710f142bc90c69b541d13
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2018-02-19 11:57:19 +0000

    Fix reference to MockitoSugar

--- src/test/scala/com/gu/editorial/permissions/client/PermissionsStoreTest.scala
+++ src/test/scala/com/gu/editorial/permissions/client/PermissionsStoreTest.scala
@@ -7,7 +7,7 @@ import org.mockito.Mockito._
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.{BeforeAndAfterAll, FunSuite}
 import org.scalatest.Matchers
-import org.scalatest.mock.MockitoSugar
+import org.scalatest.mockito.MockitoSugar
 import org.mockito.Mockito.when
 import scala.concurrent.ExecutionContext.Implicits.global
 
